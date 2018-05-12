322ed9735a8157e04f7a07d6047d837509430047
Andres Felipe Zapata Rozo <andrez.fzr@hotmail.com>
2018-05-12 11:02:01 -0500

    Cambiando procedimiento leer, ahora devuelve un Vector

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -2,58 +2,54 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Vector;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class ReadExcel {
-
-	public class LeerExcel{
-		/**
-		 * Procedimiento que lee cada una de las celdas de un archivo Excel (.xlsx)
-		 * e imprime el contenido de cada una de las celdas
-		 * @param ruta 
-		 */
-		public void leer(String ruta) {
 	
+		/**
+		 * Metodo que lee la informacion de las celdas de un archivo de Excel (.xlsx)
+		 * y guarda esa informacion dentro de un Vector 
+		 * @param ruta Path del archivo .xlsx a leer
+		 * @return Vector con la informacion de las celdas;
+		 */
+	public Vector <Cell> leer(String ruta) {
+			
+		Vector<Cell> data = new Vector<Cell>();
+	
+		try {
+			File file = new File(ruta);
+			FileInputStream excel = new FileInputStream(file);
 			try {
-				File file = new File(ruta);
-				FileInputStream excel = new FileInputStream(file);
-				try {
-					XSSFWorkbook workbook = new XSSFWorkbook(excel); // abre el archivo
-					XSSFSheet hoja = workbook.getSheetAt(0); // abre la primera hoja
+				XSSFWorkbook workbook = new XSSFWorkbook(excel); // abre el archivo
+				XSSFSheet hoja = workbook.getSheetAt(0); // abre la primera hoja
 					
-					int numFilas = hoja.getLastRowNum(); //lee el numero de filas
+				int numFilas = hoja.getLastRowNum(); //lee el numero de filas
 					
-					for(int i = 0; i < numFilas; i++) {
-						Row fila = hoja.getRow(i);
-						int numceldas = fila.getLastCellNum(); //lee el numero de celdas en la fila i
+				for(int i = 0; i < numFilas; i++) {
+					Row fila = hoja.getRow(i);
+					int numceldas = fila.getLastCellNum(); //lee el numero de celdas en la fila i
 						
-						for (int x = 0; x < numceldas; x++) {
-							Cell celda = fila.getCell(x);
-							//Imprime segun el contenido de la celda 
-							switch (celda.getCellTypeEnum().toString()) {
-							case "NUMERIC":
-								System.out.print(celda.getNumericCellValue() + " ");
-								break;
-							case "STRING":
-								System.out.println(celda.getStringCellValue() + " ");
-								break;
-							}
+					for (int x = 0; x < numceldas; x++) {
+						Cell celda = fila.getCell(x);
+						data.add(celda);
 						}
-						System.out.println(""); //para hacer salto de linea
-					} 
-					workbook.close();// cierra el archivo
+					}
 					
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			} catch (FileNotFoundException e) {
+				workbook.close(); // cierra el archivo
+					
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+			
+		return data;
 	}
+
 }
